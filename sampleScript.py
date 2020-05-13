3c216a922aaf4612921124d6868f478f270118ad
Varun <goodwillvb@gmail.com>
2020-05-13 11:41:56 -0700

    removed a bunch of commands

--- sampleScript.py
+++ sampleScript.py
@@ -2,9 +2,7 @@ import numpy as np
 import pandas as pd
 
 my_matrix = np.eye(4)
-my_second_matrix = np.array[[1,2,3,4], [2,3,4,5],[4,0,9,8],[2,3,6,6], ['Hello', 'Goodbye','Varun',
-                                                                      'Bhalla']]
+my_second_matrix = np.array[[1,2,3,4], [2,3,4,5],[4,0,9,8],[2,3,6,6], ['Hello', 'Goodbye','Monika',
+                                                                      'Handa']]
 
 print(my_matrix)
-print(my_second_matrix)
-print('Varun')
