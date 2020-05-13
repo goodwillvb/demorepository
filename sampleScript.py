3c0d459aaccd9ea9173c293cc4cfdc8b85be7215
Varun <goodwillvb@gmail.com>
2020-05-13 11:11:37 -0700

    Restored initial version and added a print statement

--- sampleScript.py
+++ sampleScript.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 my_matrix = np.eye(4)
-my_second_matrix = np.array[[1,2,3,4], [5,4,3,2],[4,0,9,8]]
+my_second_matrix = np.array[[1,2,3,4], [2,3,4,5],[4,0,9,8],[2,3,6,6]]
 
 print(my_matrix)
 print(my_second_matrix)
+print('Varun')
